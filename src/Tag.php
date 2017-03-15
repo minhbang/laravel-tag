f3ef585ac0429e6afcaadad9f769e2c7b67ce99b
Minh Bằng <contact@minhbang.com>
2017-03-15 13:29:51 +0700

    update

---
+++ src/Tag.php
@@ -0,0 +1,73 @@
+<?php namespace Minhbang\Tag;
+
+use Eloquent;
+
+/**
+ * Class Tag
+ *
+ * @package Minhbang\Tag
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Query\Builder|\Minhbang\Tag\Tag used()
+ * @method static \Illuminate\Database\Query\Builder|\Minhbang\Tag\Tag usedBy($model)
+ * @method static \Illuminate\Database\Query\Builder|\Minhbang\Tag\Tag whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Minhbang\Tag\Tag whereName($value)
+ */
+class Tag extends Eloquent
+{
+    protected $table = 'tags';
+
+    protected $fillable = ['name'];
+
+    public $timestamps = false;
+
+    /**
+     * Xóa các tags không còn dùng ở bất kỳ model nào?
+     */
+    public static function deleteUnused()
+    {
+        static::leftJoin('taggables', function ($join) {
+            /** @var \Illuminate\Database\Query\JoinClause $join */
+            $join->on('taggables.tag_id', '=', 'tags.id')->whereNull('taggables.tag_id');
+        })->delete();
+    }
+
+    /**
+     * Các tags có sử dụng
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUsed($query)
+    {
+        return $query->join('taggables', 'taggables.tag_id', '=', 'tags.id');
+    }
+
+    /**
+     * Các tags có sử dụng cho $model
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param Taggable|string $model
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUsedBy($query, $model)
+    {
+        /** @var Taggable $model */
+        $model = is_string($model) ? new $model : $model;
+
+        return $this->scopeUsed($query)->where('taggables.taggable_type', $model->getMorphClass());
+    }
+
+    /**
+     * @param string|array $names
+     * @param array $columns
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public static function findByNames($names, $columns = ['*'])
+    {
+        return static::whereIn('name', mb_array_sure($names))->get($columns);
+    }
+}
