2bcbf4fad344818e00484e6dae9a0e26596b782c
Minh Bang <contact@minhbang.com>
2017-11-07 14:34:12 +0700

    update

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,29 +9,35 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
  *
  * @package Minhbang\Tag
  */
-class ServiceProvider extends BaseServiceProvider {
+class ServiceProvider extends BaseServiceProvider
+{
     /**
      * Perform post-registration booting of services.
      */
-    public function boot() {
-        $this->loadTranslationsFrom( __DIR__ . '/../lang', 'tag' );
-        $this->loadViewsFrom( __DIR__ . '/../views', 'tag' );
-        $this->loadMigrationsFrom( __DIR__ . '/../database/migrations' );
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'tag');
+        $this->loadViewsFrom(__DIR__ . '/../views', 'tag');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->publishes(
             [
-                __DIR__ . '/../views'          => base_path( 'resources/views/vendor/tag' ),
-                __DIR__ . '/../lang'           => base_path( 'resources/lang/vendor/tag' ),
-                __DIR__ . '/../config/tag.php' => config_path( 'tag.php' ),
+                __DIR__ . '/../views' => base_path('resources/views/vendor/tag'),
+                __DIR__ . '/../lang' => base_path('resources/lang/vendor/tag'),
+                __DIR__ . '/../config/tag.php' => config_path('tag.php'),
             ]
         );
-        app( 'layout' )->registerWidgetTypes( config( 'tag.widgets' ) );
+        if (app()->isAlias('layout')) {
+            app('layout')->registerWidgetTypes(config('tag.widgets'));
+        }
     }
+
     /**
      * Register any package services.
      *
      * @return void
      */
-    public function register() {
-        $this->mergeConfigFrom( __DIR__ . '/../config/tag.php', 'tag' );
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/tag.php', 'tag');
     }
 }
